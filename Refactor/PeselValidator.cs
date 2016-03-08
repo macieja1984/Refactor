b039e29fb6868431e07bfc7a13a4bff7514e9557
Maciej Samujło <maciej.samujlo@gmail.com>
2016-03-08 22:17:26 +0100

    Project set-up

---
+++ Refactor/PeselValidator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Refactor
+{
+    public class PeselValidator
+    {
+        public bool PeselValid(string pesel)
+        {
+            var peselValid = false;
+
+            if (pesel == null)
+            {
+                peselValid = false;
+            }
+            else if (pesel.Length != 11)
+            {
+                peselValid = false;
+            }
+            else if (!pesel.All(Char.IsDigit))
+            {
+                peselValid = false;
+            }
+            else if (IsAllZeros(pesel))
+            {
+                peselValid = false;
+            }
+            else if (DateFromPesel(pesel) == null)
+            {
+                peselValid = false;
+            }
+            else
+            {
+                peselValid = PeselAlgorithmValid(pesel);
+            }
+
+            return peselValid;
+        }      
+
+
+        public bool PeselAlgorithmValid(string value)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            char[] peselDigits = value.ToCharArray();
+            long sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += long.Parse(peselDigits[i].ToString()) * weights[i];
+            }
+
+            long controlDigit = long.Parse(peselDigits.Last().ToString());
+
+            sum = (10 - (sum % 10)) % 10;
+
+            return (sum == controlDigit);
+        }
+
+
+        public DateTime? DateFromPesel(string value)
+        {
+            try
+            {
+                Dictionary<int, int> centuryFromMonthDict = new Dictionary<int, int>() { { 0, 1900 }, { 1, 1900 }, { 2, 2000 }, { 3, 2000 }, { 4, 2100 }, { 5, 2100 }, { 6, 2200 }, { 7, 2200 }, { 8, 1800 }, { 9, 1800 } };
+
+                string datePesel = value.Substring(0, 6);
+                int monthCenturyId = int.Parse(datePesel.Substring(2, 1));
+                int monthPesel = monthCenturyId % 2 == 0 ? int.Parse(datePesel.Substring(3, 1)) : int.Parse(string.Format("1{0}", datePesel.Substring(3, 1)));
+                int dayPesel = int.Parse(datePesel.Substring(4, 2));
+                int yearPesel = centuryFromMonthDict[monthCenturyId] + int.Parse(datePesel.Substring(0, 2));
+                DateTime dateFromPesel = new DateTime(yearPesel, monthPesel, dayPesel);
+                return dateFromPesel;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public bool IsAllZeros(string value)
+        {
+            return !value.Any(v => v != '0');
+        }
+
+    }
+}
---
+++ RefactorTests/PeselValidatorTests.cs
@@ -0,0 +1,80 @@
+﻿using Xunit;
+using Refactor;
+
+namespace RefactorTests
+{
+    public class PeselValidatorTests
+    {
+        private PeselValidator sut;
+        
+        [Theory]
+        [InlineData("59060202697")]
+        [InlineData("71010211771")]
+        [InlineData("10210400653")]
+        public void GivenPeselValidator_WhenCheckingCorrectPESEL_ThenResultIsTrue(string pesel)
+        {
+            sut = new PeselValidator();
+
+            var result = sut.PeselValid(pesel);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GivenPeselValidator_WhenCheckingNonNumericPESEL_ThenResultIsFalse()
+        {
+            var pesel = "abcdeabcdef";
+            sut = new PeselValidator();
+
+            bool result = sut.PeselValid(pesel);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("5906020")]
+        [InlineData("710102117711212")]
+        [InlineData("")]
+        public void GivenPeselValidator_WhenCheckingIncorrectLengthPESEL_ThenResultIsFalse(string pesel)
+        {
+            sut = new PeselValidator();
+
+            var result = sut.PeselValid(pesel);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GivenPeselValidator_WhenCheckingIncorrectDateInPESEL_ThenResultIsFalse()
+        {
+            var pesel = "23781903775";
+            sut = new PeselValidator();
+
+            var result = sut.PeselValid(pesel);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GivenPeselValidator_WhenCheckingAllZerosPESEL_ThenResultIsFalse()
+        {
+            var pesel = "00000000000";
+            sut = new PeselValidator();
+
+            var result = sut.PeselValid(pesel);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void GivenPeselValidator_WhenCheckingNullPESEL_ThenResultIsFalse()
+        {
+            sut = new PeselValidator();
+
+            var result = sut.PeselValid(null);
+
+            Assert.False(result);
+        }             
+    }
+}
+
