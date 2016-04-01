efb04b6bc60545a1c48fdd32bdd4a546f963288c
Maciej Samujło <maciej.samujlo@gmail.com>
2016-04-01 21:19:47 +0200

    Regions and mutliple classes in file

---
+++ Refactor/MutliClassFile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor
+{
+    public class MutliClassFile
+    {
+    }
+
+    #region Person
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Address PersonAddress { get; set; }
+        public Phone PersonPhone { get; set; }
+    }
+
+    #endregion
+
+    #region Address
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public int HouseNumber { get; set; }
+        public int? FlatNumber { get; set; }
+    }
+
+    #endregion
+
+    #region Phone
+
+    public class Phone
+    {
+        public PhoneType Type { get; set; }
+        public long Number { get; set; }
+    }
+
+    #endregion
+
+    #region PhoneType
+
+    public enum PhoneType
+    {
+        Mobile,
+        Landline,
+        Sat        
+    }
+
+    #endregion
+
+}
