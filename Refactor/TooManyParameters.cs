7337b7f1dbd775a001d0b4fad891e94bb8c40e03
Maciej Samujło <maciej.samujlo@gmail.com>
2016-04-01 21:30:39 +0200

    Too many parameters in function

---
+++ Refactor/TooManyParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor
+{
+    public class TooManyParameters
+    {
+        public int CalculateCarInsuranceRate(int DriversAge, DateTime DriversLicenceIssueDate, 
+            bool DriverIsMarried, bool DriverHasChildren, 
+            int DriversAccidentsNumber, DateTime CarsProductionDate,
+            int CarsValue, string CarsMake, 
+            string CarsModel, bool CarIsParkedInGarage)
+        {
+            // business logic
+
+            return 123;
+        }
+
+    }
+
+
+}
